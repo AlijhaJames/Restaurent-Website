522fe6face3393533f1f3331f8898a6f626c1f86
Alijha James <138966217+AlijhaJames@users.noreply.github.com>
2024-07-02 16:55:17 +0530

    Update Reservation.jsx

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -18,7 +18,8 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        // "http://localhost:4000/api/v1/reservation/send",
+        "https://restaurent-website-1.onrender.com",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
